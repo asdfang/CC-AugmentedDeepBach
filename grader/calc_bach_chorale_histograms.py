d57c01c57a0843cf6af08dc9e1b172d40d808207
Alexander Fang <alex.s.fang@gmail.com>
2019-11-21 19:29:26 -0600

    Created script to get note and rhythm histograms over original Bach chorales.

---
+++ grader/calc_bach_chorale_histograms.py
@@ -0,0 +1,57 @@
+import music21
+import collections
+from tqdm import tqdm
+
+# from DeepBach
+def is_valid(chorale):
+  if not len(chorale.parts) == 4:
+    return False
+  return True
+
+def chorale_note_histogram(chorale, key):
+  nh = collections.Counter()
+  for note_or_rest in chorale.flat.notesAndRests:
+    if note_or_rest.isNote:
+      sd = key.getScaleDegreeAndAccidentalFromPitch(note_or_rest.pitch)
+      nh[sd] += 1
+    else:
+      nh['Rest'] += 1
+  return nh
+
+def chorale_rhythm_histogram(chorale, i):
+  rh = collections.Counter()
+  for note_or_rest in chorale.flat.notesAndRests:
+    rh[note_or_rest.duration.quarterLength] += 1
+    if note_or_rest.duration.quarterLength == 0.0:
+      print(i)
+      chorale.show()
+      chorale.show('txt')
+  return rh
+
+#208, 270
+major_nh = collections.Counter()
+minor_nh = collections.Counter()
+all_rh = collections.Counter()
+for i, fname in tqdm(enumerate(music21.corpus.chorales.Iterator(209, 209, returnType='filename'))):
+  chorale = music21.corpus.parse(fname)
+  if is_valid(chorale):
+    key = chorale.analyze('key')
+
+    # note histograms
+    chorale_nh = chorale_note_histogram(chorale, key)
+    if key.mode == 'major':
+      major_nh += chorale_nh
+    else:
+      minor_nh += chorale_nh
+
+    # rhythm histogram
+    all_rh += chorale_rhythm_histogram(chorale, i)
+
+    
+
+print('major:')
+print(major_nh)
+print('minor:')
+print(minor_nh)
+print('rhythm:')
+print(all_rh)
